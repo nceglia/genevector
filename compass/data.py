000e845a2913e9f0ffd9e7af3c38863b2e974e27
Nick Ceglia <nickceglia@gmail.com>
2022-04-05 21:23:33 -0400

    removing logged

--- compass/data.py
+++ compass/data.py
@@ -264,7 +264,7 @@ class CompassDataset(Dataset):
                     value = self.mi_scores[gene][cgene]
                 value = value * coocc[wi,ci]
                 if value > 0:
-                    self._xij.append(numpy.log2(value))
+                    self._xij.append(value)
                 else:
                     self._xij.append(0.0)
         if self.device == "cuda":

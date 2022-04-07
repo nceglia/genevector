1fa74720bdc7d918365dc3108a9971eed26c26db
Nick Ceglia <nickceglia@gmail.com>
2022-04-07 00:05:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='compass',
+    name='genevector',
     version='0.0.1',
     description='Single Cell Gene Vector Library',
-    packages=find_packages(include=['compass']),
-    #install_requires=["scipy","numpy","sklearn","torch","pandas","scanpy","umap","tqdm","seaborn"],
+    packages=find_packages(include=['genevector']),
+    install_requires=["scipy","numpy","sklearn","torch","pandas","scanpy","umap-learn","tqdm","seaborn","matplotlib"],
 )

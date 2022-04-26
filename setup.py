a2f9c4618b94bebce6525c08f08d2773e9fbe83e
Nick Ceglia <nickceglia@gmail.com>
2022-04-25 20:12:21 -0400

    adding cython

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0.1',
     description='Single Cell Gene Vector Library',
     packages=find_packages(include=['genevector']),
-    install_requires=["scipy","numpy","sklearn","torch","pandas","scanpy","umap-learn","tqdm","seaborn","matplotlib"],
+    install_requires=["scipy","numpy","sklearn","cython","pandas","scanpy","umap-learn","tqdm","seaborn","matplotlib"],
 )

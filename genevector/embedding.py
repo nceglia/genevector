31447e833b78cd5c6899a9889ab918d0819384ff
Nick Ceglia <nickceglia@gmail.com>
2022-04-29 12:46:05 -0400

    feature range instead of adding back 1

--- genevector/embedding.py
+++ genevector/embedding.py
@@ -322,8 +322,8 @@ class CellEmbedding(object):
                     weights.append(w)
             weights = numpy.array(weights)
             scaler = MinMaxScaler()
-            scaled_weights = scaler.fit_transform(numpy.array(weights).reshape(-1,1))
-            scaled_weights = [x + 1.0 for x in list(scaled_weights.reshape(1,-1)[0])]
+            scaled_weights = scaler.fit_transform(numpy.array(weights).reshape(-1,1),feature_range=(1.0,3.0))
+            # scaled_weights = [x + 1.0 for x in list(scaled_weights.reshape(1,-1)[0])]
             vectors = numpy.array([embed.embeddings[gene] for gene in cell_genes])
             self.matrix.append(numpy.average(vectors,axis=0,weights=scaled_weights))
             self.data[cell] = vectors

6d5a6844d8eea9aa4fdf419826cb98da6c157f5a
Nick Ceglia <ceglian@mskcc.org>
2023-11-05 21:10:41 -0500

    Removing html theme

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,5 @@ add_module_names = False
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "../logo.png"

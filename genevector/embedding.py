6c24dac887b3b9d0bbee03382741e47364d84306
Nick Ceglia <nickceglia@gmail.com>
2023-12-11 23:16:37 -0500

    update prob plot to have filename arg

--- genevector/embedding.py
+++ genevector/embedding.py
@@ -593,12 +593,12 @@ class CellEmbedding(object):
         ax.set_ylim(0,1)
         ax.set_title("Probability vs Module Score (r2)")
 
-    def plot_probabilities(self,adata):
+    def plot_probabilities(self,adata,save=filename):
         prob_cols = []
         for x in adata.obs.columns:
             if "Pseudo-probability" in x:
                 prob_cols.append(x)
-        sc.pl.umap(adata,color=prob_cols,s=30,ncols=3,cmap="magma",alpha=0.8)
+        sc.pl.umap(adata,color=prob_cols,s=30,ncols=3,cmap="magma",alpha=0.8,save=save)
 
     def cell_distance(self, vec, norm=True):
         if norm:

ca7eccf94db40910821bb954f7de8d9ab28c57a8
Nick Ceglia <nickceglia@gmail.com>
2026-04-05 23:50:49 -0400

    removing setup

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='genevector',
-    version='1.0.0',
-    description='Single Cell GeneVector Library',
-    packages=find_packages(include=['genevector']),
-)
